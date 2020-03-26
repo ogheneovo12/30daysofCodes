08f0d7255eebbc0dcba5f9f61071026bd889f862
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-03-26 20:21:00 +0100

    finished day1, added  command line functionality run command is node beday1 [pass in arguement or not] optional

---
+++ beday1.js
@@ -0,0 +1,36 @@
+//check if arguement was read from the command line if not ask for it
+const input = process.argv.slice(2).map(Number);
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+})
+
+const testSign = (argv)  =>{
+   switch(Math.sign(argv)){
+      case 1:
+          return `${argv}:positive`;
+          break;
+      case -1:
+          return `${argv}:negative`;
+          break; 
+     case 0: 
+     return `${argv}: +/-0`;
+     break;    
+      default:
+          return "please enter a valid Number"
+          break;
+   }
+}
+//this is to pass command from the command line
+if (input.length > 0 ){
+    input.forEach(arg =>console.log(testSign(arg)))
+    rl.close();// close stdin 
+}else{
+    rl.question('please enter numbers seperated by space:', (input) => {
+        // TODO: Log the answer in a databa;
+        input = input.split(' ').map(Number);
+        input.forEach(arg =>console.log(testSign(arg)))
+        rl.close();
+      });
+}
