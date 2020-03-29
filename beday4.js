7ba17d22d25449b65512345efd6151d3ee335da9
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-03-29 15:19:24 +0100

    did nothing

--- beday4.js
+++ beday4.js
@@ -2,7 +2,7 @@ const equalOccurence = (arr1, arr2) => {
   //check for which array is longer
   const alreadyCheckedValues = [];
   const longerArray = arr1.length > arr2.length ? arr1 : arr2;
-  const shorterArray = arr1.length < arr2.length ? arr1 : arr2;
+  const shorterArray = arr1.length < arr2.length ? arr2 : arr1;
   const elemThatOccuredTwice = shorterArray.filter(el => {
     if (longerArray.includes(el) && !alreadyCheckedValues.includes(el)) {
       alreadyCheckedValues.push(el);
@@ -11,4 +11,4 @@ const equalOccurence = (arr1, arr2) => {
   });
   console.log(elemThatOccuredTwice);
 };
-equalOccurence([1, 4, 5, 7, 7, 8], [1, 1, 5, 5, 6, 7]);
+equalOccurence([1,1,0], [1, 1,]);
