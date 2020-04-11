923bdcec7d0933250a5fcee438959e7b8d5b61c8
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-04-11 03:09:19 +0100

    finished day17

--- beday17.js
+++ beday17.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
+const JWT_KEY = process.env.JWT_KEY || "30_DAYSOF_CODE@_DSC_@2020";
 const PORT = process.env.PORT || 3000;
+const jwt = require("jsonwebtoken");
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,13 +11,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let USERS = [];
 const saltRounds = 12;
 class User {
-  constructor(name, email, password) {
+  constructor(name, email, password, token) {
     this.name = name;
     this.email = email;
     this.password = password;
     this.isLoggedIn = false;
+    this.tokens = [];
+    this.tokenLife = 5 * 60; //5 minutes
+    this.signedUp_at = new Date().toLocaleString();
   }
-
+  generateAuthToken() {
+    const token = jwt.sign({ _id: this.email }, JWT_KEY, {
+      expiresIn: this.tokenLife,
+    });
+    this.tokens = this.tokens.concat({ token });
+    return token;
+  }
   getPassWord() {
     return this.password;
   }
@@ -63,10 +74,50 @@ function validateInput(arr, callback) {
   });
   return error;
 }
+/************************************************
+ *************** MIDDLE WARES *****************/
+const authoriser = (req, res, next) => {
+  let sender = {};
+  let token = req.header("Authorization");
+  if (token) {
+    token = token.replace("Bearer ", "");
+  } else {
+    return res
+      .status(401)
+      .send({ AUTH: false, message: "Not authorized to access this resource" });
+  }
+  //i would have loved to use the callback but i was avoiding too much callback
+  //so i used try..catch instead
+  try {
+    sender = jwt.verify(token, JWT_KEY);
+  } catch (error) {
+    if (error.message.startsWith("jwt")) {
+      error.message = error.message.replace("jwt", "token");
+    }
+    return res.status(401).send({ AUTH: false, message: error.message });
+  }
+  const user = USERS.find(
+    (user) =>
+      user.email == sender._id && user.tokens.filter((tk) => tk.token == token)
+  );
+  //token must match the user, if not prevent access
+  if (!user || req.body.email != user.email) {
+    return res
+      .status(401)
+      .send({ AUTH: false, error: "Not authorized to access this resource" });
+  }
+  const { name, email, signedUp_at } = user;
+  req.user = { name, email, signedUp_at };
+  req.token = token;
+  next();
+};
+
+/************************************************
+ *************** SIMPLY_ROUTERS  *****************/
 
 app.post("/login", (req, res) => {
   let user = USERS.find(
-    (user) => user.email == req.body.email || user.name == req.body.username
+    (user) => user.name == req.body.username || user.email == req.body.email
   );
   if (!user) {
     return res.json({
@@ -80,8 +131,10 @@ app.post("/login", (req, res) => {
     }
     if (result) {
       user.logUserIn();
+      const token = user.generateAuthToken();
       return res.json({
-        success: "true",
+        AUTH: "true",
+        token,
         message: "you have been succesfully logged in",
       });
     }
@@ -111,7 +164,8 @@ app.post("/signup", (req, res) => {
     if (err) {
       return res.json({ success: "false", err });
     } else {
-      USERS.push(new User(req.body.username, req.body.email, hash));
+      const user = new User(req.body.username, req.body.email, hash);
+      USERS.push(user);
       return res.json({
         success: "true",
         message: "Account has been succesfully registered",
@@ -119,6 +173,8 @@ app.post("/signup", (req, res) => {
     }
   });
 });
-
+app.get("/user/profile", authoriser, (req, res) => {
+  res.json(req.user);
+});
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
-//Voila and i finally DID IT, but i never chop since morning 7:02pm now
+//Voila and i finally DID IT, but i never chop since morning 7:02pm nowno
